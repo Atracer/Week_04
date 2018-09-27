2621f7cb910ab122d5c0f88c122613751a28412c
peteryu <jiming2@illinois.edu>
2018-09-26 20:07:58 -0500

    remove button

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import ToDoListItem from "./components/toDoListItem";
 
 class App extends Component {
   state = {
-    todoListTitle: "My Todo List",
-    todos: ["Walk to latea", "Get boba"]
+    todoListTitle: "My Codo List",
+    todos: ["Walk to CS196", "Get pineapple"]
   };
-
-  addParameter = () => {
+  removeParameter = () => {
     this.setState({
-      todos: [...this.state.todos, "Get more boba"]
+      todos: [],
+      todoListTitle: []
     })
   };
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.todoListTitle}</h1>
-        {this.state.todos.map(x => (
-          <ToDoListItem todo={x} />
-        ))}
-        <button onClick={this.addParameter}> Add todo</button>
-      </div>
-    );
+render() {
+  return (
+    <div>
+      <h1>{this.state.todoListTitle}</h1>
+      {this.state.todos.map(x => (
+        <ToDoListItem todo={x} />
+      ))}
+      <button onClick={this.removeParameter}> remove</button>
+    </div>
+  )
   }
 }
+
 
 export default App;
